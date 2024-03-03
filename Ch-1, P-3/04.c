a9b0074bf2e276df29ccdab6bfa144a1f9a7812d
Shakil Shahadat <shakil.regi@gmail.com>
2024-03-03 23:13:04 +0600

    Update 04.c

--- Ch-1, P-3/04.c
+++ Ch-1, P-3/04.c
@@ -1,9 +1,16 @@
 #include <windows.h>
 
-int WINAPI WinMain ( HINSTANCE hInstance, HINSTANCE hPrevInstance,
+int WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance,
 					PSTR szCmdLine, int iCmdShow )
 {
-	MessageBox ( NULL, TEXT( "Hello, Shakil!" ), TEXT( "Welcome!" ), 0 );
+	MessageBox( NULL, TEXT( "Hello, Shakil!" ), TEXT( "Welcome!" ), 0 );
+
+	// Only ASCII is supported, not UNICODE.
+	// ToDo: Find out how can I use UNICODE.
+	// MessageBox( NULL, TEXT( "হ্যালো, শাকিল!" ), TEXT( "স্বাগতম!" ), 0 );
+
+	// When clicked on the produced exe, a command prompt also opens.
+	// ToDo: Find a way that doesn't produce command prompt.
 
 	return 0;
 }
